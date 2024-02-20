75a441c2f161313639e242ed003a9f176cf1ae25
Jonacodez <75742967+Jonacodez@users.noreply.github.com>
2024-02-20 19:49:19 +0100

    Update main.cpp

--- Lab 1 /main.cpp
+++ Lab 1 /main.cpp
@@ -1,25 +1,36 @@
 #include <iostream>
 #include "computer.h"
-//using namespace std;
+#include "program.h"
+using namespace std;
+
 int main()
 {
-    Computer Computer1;
-    Computer1.cpiArith = 2;
-    Computer1.cpiBranch = 4;
-    Computer1.cpiLoad = 3;
-    Computer1.cpiStore = 2;
-    Computer1.clockRateGHz = 1;
+    Program ProgramA(2000, 100, 100, 50), ProgramB(2000, 0.1, 0.4, 0.25), ProgramC(500, 100, 2000, 200);
+    ProgramA.printStats();
+    ProgramB.printStats();
+    ProgramC.printStats();
+    Computer ComputerA(1, 2, 2, 3, 4), ComputerB(1.2, 2, 3, 4, 3), ComputerC(2, 2, 2, 4, 6);
+    ComputerA.printStats();
+    ComputerA.printStats();
+    ComputerB.printStats();
+    ComputerC.printStats();
 
-    Program Program1;
-    Program1.numArith = 2000;
-    Program1.numBranch = 50;
-    Program1.numLoad = 100;
-    Program1.numStore = 100;
+    printf("%lf\n", ComputerA.calculateGlobalCPI(ProgramA));
+    printf("%lf\n", ComputerA.calculateGlobalCPI(ProgramB));
+    printf("%lf\n", ComputerA.calculateGlobalCPI(ProgramC));
 
-    printf("Computer1: %f\n",Computer1.calculateGlobalCPI(Program1));
-    Computer1.printStats();
+    printf("%lf\n", ComputerA.calculateExecutionTime(ProgramA));
+    printf("%lf\n", ComputerA.calculateExecutionTime(ProgramB));
+    printf("%lf\n", ComputerA.calculateExecutionTime(ProgramC));
 
+    printf("%lf\n", ComputerA.calculateMIPS(ProgramA));
+    printf("%lf\n", ComputerA.calculateMIPS(ProgramB));
+    printf("%lf\n", ComputerA.calculateMIPS(ProgramC));
+    //Computer Computer1(program1);
 
+    //Program Program1(2000, 50, 100, 100);
 
+    //printf("Computer1: %f\n",Computer1.calculateGlobalCPI(Program1));
+    //Computer1.printStats();
     return 0;
 };
