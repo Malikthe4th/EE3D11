2c9b07ee7a084ad7170b0f74d1e611eabcfc9f62
Jonacodez <75742967+Jonacodez@users.noreply.github.com>
2024-02-20 19:49:04 +0100

    Update program.h

--- Lab 1 /program.h
+++ Lab 1 /program.h
@@ -1,21 +1,31 @@
+#pragma once
 class Program {
     public:
         //program aspect
-        int numArith; //Arithmetic and logic calculations
-        int numStore; //Store data into memory
-        int numLoad; //Load data from memory
-        int numBranch; //Conditional and unconditional jumps
-        int numTotal; //Total number of instructions
+        int numArith = 0; //Arithmetic and logic calculations
+        int numStore = 0; //Store data into memory
+        int numLoad = 0; //Load data from memory
+        int numBranch = 0; //Conditional and unconditional jumps
+        int numTotal = 0; //Total number of instructions
 
-        Program(int, int, int, int) {
-            numArith = 0; //Arithmetic and logic calculations
-            numStore = 0; //Store data into memory
-            numLoad = 0; //Load data from memory
-            numBranch = 0; //Conditional and unconditional jumps
+        Program(int x, int y, int z, int a) {
+            numArith = x; //Arithmetic and logic calculations
+            numStore = y; //Store data into memory
+            numLoad = z; //Load data from memory
+            numBranch = a; //Conditional and unconditional jumps
+            numTotal = x + y + z + a;
         }
 
-        void printStats()
-        {
-            printf("Arith:\t%d\nStore:\t%d\nLoad:\t%d\nBranch:\t%d\n\nTotal:\t%d\n",numArith,numStore,numLoad,numBranch,numTotal);
+        Program(int x, double y, double z, double a) {
+            numTotal = x;
+            numArith = y*x; //Arithmetic and logic calculations
+            numStore = z*x; //Store data into memory
+            numLoad = a*x; //Load data from memory
+            numBranch = (1 - y - z - a)*x; //Conditional and unconditional jumps
+        }
+
+
+        void printStats() {
+            printf("\nArith:\t%d\nStore:\t%d\nLoad:\t%d\nBranch:\t%d\n\nTotal:\t%d\n\n", numArith,numStore,numLoad,numBranch,numTotal);
         };
 };
