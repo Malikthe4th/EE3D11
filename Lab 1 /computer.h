66ad0844849fb073dabac1891e5a04ed2e7dd2cb
Jonacodez <75742967+Jonacodez@users.noreply.github.com>
2024-02-20 19:45:47 +0100

    Update computer.h

--- Lab 1 /computer.h
+++ Lab 1 /computer.h
@@ -1,46 +1,47 @@
+#pragma once
+#include "program.h"
+
 class Computer {
     public:
         //computer aspect
-       double clockRateGHz; //Clock rate in GHz
-       double cpiArith; //CPI of instruction class Arith
-       double cpiStore; //CPI of instruction class Store
-       double cpiLoad; //CPI of instruction class Load
-       double cpiBranch; //CPI of instruction class Branch
-        Computer(double, double, double, double, double) {
-            clockRateGHz = 0;
-            cpiArith = 0;
-            cpiStore = 0;
-            cpiLoad = 0;
-            cpiBranch = 0;
+        //#include "program.h"
+        double clockRateGHz; //Clock rate in GHz
+        double cpiArith; //CPI of instruction class Arith
+        double cpiStore; //CPI of instruction class Store
+        double cpiLoad; //CPI of instruction class Load
+        double cpiBranch; //CPI of instruction class Branch
+
+        double numArith = 0; //Arithmetic and logic calculations
+        double numStore = 0; //Store data into memory
+        double numLoad = 0; //Load data from memory
+        double numBranch = 0; //Conditional and unconditional jumps
+        int numTotal = 0; //Total number of instructions
+
+        Computer(double x, double y, double z, double a, double b) {
+            clockRateGHz = x;
+            cpiArith = y;
+            cpiStore = z;
+            cpiLoad = a;
+            cpiBranch = b;
         }
+
        void printStats()
        {
-            printf("clockRateGHz:\t%f\ncpiArith:\t%f\ncpiStore:\t%f\ncpiLoad:\t%f\ncpiBranch:\t%f\n",clockRateGHz,cpiArith,cpiStore,cpiLoad,cpiBranch);
+            printf("clockRateGHz:\t%f\ncpiArith:\t%f\ncpiStore:\t%f\ncpiLoad:\t%f\ncpiBranch:\t%f\n\n", clockRateGHz,cpiArith,cpiStore,cpiLoad,cpiBranch);
        };
        
-       double calculateGlobalCPI(int numArith, int numStore, int numLoad, int numBranch, int numTotal)
+       double calculateGlobalCPI(Program& data)
        {
-        if (numTotal >= 0)
-        {
-            printf("not null\n");
-            return ((numArith*cpiArith) + (numBranch*cpiBranch) + (numLoad*cpiLoad) + (numStore*cpiStore));
-            
-        }
-        else
-        {
-            printf("isnull\n");
-            numTotal = numArith + numBranch + numLoad + numStore;
-            return (((numArith*cpiArith) + (numBranch*cpiBranch) + (numLoad*cpiLoad) + (numStore*cpiStore))/numTotal);
-        };
+            return (((data.numArith*cpiArith) + (data.numBranch*cpiBranch) + (data.numLoad*cpiLoad) + (data.numStore*cpiStore))/data.numTotal);
        };
 
-       double calculateExecutionTime(int numTotal) //time in seconds
+       double calculateExecutionTime(Program& data) //time in seconds
        {
-            return (numTotal*calculateGlobalCPI()/clockRateGHz*1000000000);
+            return ((data.numTotal* this->calculateGlobalCPI(data))/(clockRateGHz*1000000000));
        };
 
-       double calculateMIPS()
+       double calculateMIPS(Program& data)
        {
-            return (clockRateGHz*1000000000/1000000*cpiArith);
+            return (clockRateGHz*1000000000/1000000*this->calculateGlobalCPI(data));
        };
 };
